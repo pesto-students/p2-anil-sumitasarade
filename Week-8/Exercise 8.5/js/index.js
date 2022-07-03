6cf5dfa624bae638f1d328a0f02b6f451b8076cb
Sumita Sarade <sumitasarade.ss@gmail.com>
2022-07-03 22:59:14 +0530

    Added all the assignments

---
+++ Week-8/Exercise 8.5/js/index.js
@@ -0,0 +1,29 @@
+const findJudge = (N, trust) => {
+    // keep track of how many likes the element gives
+    let likesCountList = {}
+    
+    let beingLikedCountList = {}
+
+    for(let i = 1; i <= N; i++){
+        likesCountList[i] = 0
+        beingLikedCountList[i] = 0
+    }
+    
+    for(let ele of trust){
+        likesCountList[ele[0]]++
+        beingLikedCountList[ele[1]]++
+
+    let judge = 0
+    for(key in likesCountList){
+        if(likesCountList[key] === 0) judge = key
+    }
+    
+    if(beingLikedCountList[judge] === N - 1) return judge
+    else return -1
+};
+
+let findJudge1 = findJudge(3, [[1,3],[2,3],[3,1]]);
+console.log(findJudge1);
+
+
+
